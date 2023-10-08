7039ec8a7e2b885061d1fe4c17819631a947e41f
Mr Lucky <115186825+LuckyChetan@users.noreply.github.com>
2023-10-08 21:20:58 +0530

    Create RemoveNode.java
    Code for Remove the n-th node from the end of a linked list.

---
+++ RemoveNode.java
@@ -0,0 +1,25 @@
+public class RemoveNthNodeFromEnd {
+    public ListNode removeNthFromEnd(ListNode head, int n) {
+        ListNode dummy = new ListNode(0);
+        dummy.next = head;
+
+        ListNode first = dummy;
+        ListNode second = dummy;
+
+        // Move first ahead by n+1 steps
+        for (int i = 0; i <= n; i++) {
+            first = first.next;
+        }
+
+        // Move first to the end, maintaining the gap
+        while (first != null) {
+            first = first.next;
+            second = second.next;
+        }
+
+        // Remove the nth node from the end
+        second.next = second.next.next;
+
+        return dummy.next;
+    }
+}
